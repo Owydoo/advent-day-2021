1e94904b42fe666e347a53300c846946a6479d93
Hugo <h.hersemeule@live.fr>
2021-12-08 17:30:55 +0100

    Day 6 exo 1 done

--- src/main/java/day6/exo1/Main.java
+++ src/main/java/day6/exo1/Main.java
@@ -2,54 +2,44 @@ package day6.exo1;
 
 import utils.Parsing;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         var parser = new Parsing();
-        String filename = "src/main/java/day6/inputs/inputsEasy.txt";
-//        String filename = "src/main/java/day6/inputs/inputs.txt";
+//        String filename = "src/main/java/day6/inputs/inputsEasy.txt";
+        String filename = "src/main/java/day6/inputs/inputs.txt";
 
         List<String> elements = parser.parseInlineTextFile(filename, ",");
         System.out.println(elements);
 
         List<Integer> startingFishs = elements.stream().map(e -> Integer.parseInt(e)).collect(Collectors.toList());
         System.out.println(startingFishs);
-        System.out.println(startingFishs.get(1).getClass().getName());
 
-        int nbDays = 18;
-        List<Integer> endFishs = simulatingLanternFishs(startingFishs, nbDays);
+        Map<Integer, Integer> integerIntegerMap = startingFishs.stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.collectingAndThen(Collectors.toList(), List::size)));
 
-        int answer = endFishs.size();
-        System.out.println("answer" + answer);
+        System.out.println(integerIntegerMap);
 
+        for (int day = 0; day < 80; day++) {
+            System.out.println("day" + day);
+            Map<Integer, Integer> newMap = new HashMap<>(integerIntegerMap.size());
+            integerIntegerMap.forEach((key, value) -> {
+                if (key == 0) {
+                    newMap.put(8, value + newMap.getOrDefault(8, 0));
+                    newMap.put(6, value + newMap.getOrDefault(6, 0));
+                } else {
+                    newMap.put(key - 1, value + newMap.getOrDefault(key - 1, 0));
+                }
+            });
+            integerIntegerMap = newMap;
+            System.out.println(integerIntegerMap);
+        }
+
+        Integer answer = integerIntegerMap.values().stream().reduce(0, Integer::sum);
+        System.out.println("answer : " + answer);
     }
 
-    private static List<Integer> simulatingLanternFishs(List<Integer> startingFishs, int nbDays) {
-        List<Integer> res = startingFishs;
-        if (nbDays == 0){
-            return res;
-        }
-        else {
-            res = countOneDay(startingFishs);
-            return simulatingLanternFishs(res, nbDays--);
-        }
-    }
-
-    private static List<Integer> countOneDay(List<Integer> startingFishs) {
-        List<Integer> res = new ArrayList<>();
-        for (Integer fish :
-                startingFishs) {
-            fish -= 1;
-            if (fish == -1){
-                fish = 6;
-                int newFish = 8;
-                res.add(newFish);
-            }
-            res.add(fish);
-        }
-        return res;
-    }
 }
